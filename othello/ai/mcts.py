fix bug in reward system, add softmax on model

othello/ai/mcts.py
@@ -195,16 +195,19 @@ class MCTS:
         for action in possible_actions:
             i,j = action
             p = policy_logits[i][j]
-            policy.append(math.exp(p))
-
-        policy_sum = sum(policy) + 1e-9
-        if policy_sum == 0:
-            print('policy_sum : ', policy_sum, possible_actions)
+            policy.append(p)
 
         for i, p in enumerate(policy):
-            node.children[possible_actions[i]] = Node(p/policy_sum)
+            node.children[possible_actions[i]] = Node(p)
         
-        self.backpropagate(self.search_path, value, self.root_turn)
+        if node.is_terminal():
+            if node.winner == node.turn:
+                self.backpropagate(self.search_path, 1, node.turn)
+            else:
+                self.backpropagate(self.search_path, -1, node.turn)
+                
+        else:
+            self.backpropagate(self.search_path, value, node.turn)
     
     # We use the neural network to obtain a value and policy prediction.
     def evaluate(self, node: Node, board: Board, nnet):
@@ -216,24 +219,16 @@ class MCTS:
         possible_actions = board.get_possible_actions(board.turn)
         policy = []
 
-        def softmax(x):
-            e_x = np.exp((x - np.max(x)))
-            return e_x / e_x.sum()
-
         for action in possible_actions:
             i,j = action
             p = policy_logits[0][0][i][j]
-            # policy.append(math.exp(p))
             policy.append(p)
 
-        # policy_sum = sum(policy) + 1e-9
-        softmax_lst = softmax(np.array(policy))
-
-        temp = 0
-        for i, sm in enumerate(softmax_lst):
-            node.children[possible_actions[i]] = Node(sm)
-            temp += sm
-        # print(policy)
+        sum_p = 0
+        for i, p in enumerate(policy):
+            node.children[possible_actions[i]] = Node(p)
+            sum_p += p
+        
         # print(softmax_lst)
         
         return float(value)

othello/ai/othello_net.py
@@ -6,9 +6,9 @@ from core.types import MAX_COL, MAX_ROW
 BOARD_C_IN = 3
 BOARD_C_OUT = 1
 
-NUM_RESIDUAL_LAYERS = 4
-NUM_RESNET_CHANNEL = 512
-HEAD_FC_SIZE = 512
+NUM_RESIDUAL_LAYERS = 5
+NUM_RESNET_CHANNEL = 256
+HEAD_FC_SIZE = 1024
 
 def initialize_weights(m):
     if isinstance(m, nn.Conv2d):
@@ -33,7 +33,7 @@ class OthelloNet(nn.Module):
 
         self.num_steps = 0
 
-        # self.apply(initialize_weights)
+        self.apply(initialize_weights)
 
     def increase_num_steps(self):
         self.num_steps += 1
@@ -51,12 +51,12 @@ class OthelloNet(nn.Module):
         return p,v
     
     def inference(self, _board):
-        # board : [MAX_ROW, MAX_COL, 3]
+        # board : [3, MAX_ROW, MAX_COL]
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
         input = torch.tensor(_board, dtype=torch.float).to(device)
         B = input.shape[0]
-        # input = input.unsqueeze(0) # [1,C,H,W]
+        # [1,H,W,C]
         p,v = self(input)
         p = torch.reshape(p, (B,BOARD_C_OUT,MAX_ROW,MAX_COL))
         p = p.cpu().detach().numpy()
@@ -84,11 +84,12 @@ class PolicyHead(nn.Module):
             nn.Linear(HEAD_FC_SIZE, HEAD_FC_SIZE),
             nn.ReLU(),
             nn.Linear(HEAD_FC_SIZE, ret_dim),
+            nn.Softmax(dim=1)
         ).to(device)
 
     def forward(self, x):
-        x=self.model(x)
-        return torch.nn.functional.log_softmax(x, dim=1)
+        x = self.model(x)
+        return x
 
 class ValueHead(nn.Module):
     def __init__(self, in_channels):

othello/ai/trainer.py
@@ -77,10 +77,11 @@ class Trainer:
         while True:
             print(f'epoch : {epoch}')
 
-            # self.selfplay_game(self.nnet, replay_buffer, file_manager)
+            self.selfplay_game(self.nnet, replay_buffer, file_manager)
             self.train_network(replay_buffer, file_manager)
             self.evaluate_vs_randomplay(self.nnet, file_manager, 100)
-            self.selfplay_game(self.nnet, replay_buffer, file_manager)
+
+            # self.selfplay_game(self.nnet, replay_buffer, file_manager)
             epoch+=1
 
     def make_replay(self):
@@ -152,6 +153,8 @@ class Trainer:
 
                 # select action
                 for i, mcts in enumerate(mcts_list):
+                    if board_list[i].is_terminal():
+                        continue
                     action = mcts.select_action(board_list[i].current_move, use_sampling=False)
                     board_list[i].take_action(action)
                     action_history[i].append(action)
@@ -312,6 +315,7 @@ class Trainer:
     
     def update_weights(self, optimizer, nnet, batch, print_loss=False):
         mse_loss = nn.MSELoss()
+        # cross_entrophy_loss = nn.functional.cross_entropy
         nll_loss = self.loss_pi
         
         image, target_policy, target_value = batch
@@ -322,7 +326,7 @@ class Trainer:
 
         policy_logits, value = nnet(image)
         loss1 = mse_loss(value, target_value)
-        loss2 = nll_loss(policy_logits, target_policy)
+        loss2 = nll_loss(torch.log(policy_logits), target_policy)
         loss = loss1+loss2
 
         optimizer.zero_grad()